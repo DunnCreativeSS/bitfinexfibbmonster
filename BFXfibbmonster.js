288299a620db38557963359bdb6ca748a056cbc7
Jarett Dunn <jarett+github@dcssquared.com>
2018-08-09 17:17:42 -0300

    chart

--- BFXfibbmonster.js
+++ BFXfibbmonster.js
@@ -1981,7 +1981,9 @@ app.get('/graph', function (req, res){
 		
 		+ '<div id="container"></div>'
 		+ '<div style="display:none;" id="prices">' + JSON.stringify(prices) + '</div>'
-+ '<script>var prices=JSON.parse($("#prices").text()),btcusd=[],pl=[];for(var p in prices){var t=prices[p].t,btcusdt=prices[p].btcusd,plt=prices[p].pl;btcusd.push([t,btcusdt]),pl.push([t,plt])}var options={rangeSelector:{buttons:[{count:1,type:"minute",text:"1m"},{count:5,type:"minute",text:"5m"},{count:7,type:"minute",text:"7m"},{count:14,type:"minute",text:"14m"},{count:21,type:"minute",text:"21m"},{type:"all",text:"All"}]},yAxis:[{title:{text:"Price"},height:"60%"}],title:{text:"Bitfinex Fibb Monster"},subtitle:{text:"By: Jarett Dunn jarettrsdunn@gmail.com"},yAxis:{title:{text:"%"}},series:[{name:"BTCUSD",data:btcusd},{name:"P/L",data:pl}]};</script>');
++ '<script>var prices=JSON.parse($("#prices").text()),btcusd=[],pl=[];for(var p in prices){var t=prices[p].t,btcusdt=prices[p].btcusd,plt=prices[p].pl;btcusd.push([t,btcusdt]),pl.push([t,plt])}var options={title:{text:"Bitfinex Margin Fib Monster"},rangeSelector:{buttons:[{count:1,type:"day",text:"1d"},{count:5,type:"day",text:"5d"},{count:7,type:"day",text:"7d"},{count:14,type:"day",text:"14d"},{count:21,type:"day",text:"21d"},{type:"all",text:"All"}],inputEnabled:!1,selected:0},yAxis:[{labels:{align:"right",x:-3},title:{text:"%"},height:"100%",lineWidth:2,resize:{enabled:!0}}],tooltip:{split:!0},series:[{name:"PL",data:pl,color:"green"},{name:"btcusd",data:btcusd,color:"black"}]};</script>'
+		+ '<script>Highcharts.stockChart("container", options);</script>');
+
 })
 			})
 			app.listen(process.env.PORT || 8081, function() {});
